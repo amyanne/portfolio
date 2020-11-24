09ed823b04917384a042d8b0eafb8108e8d7372a
amyanne <amywestcott01@gmail.com>
2020-11-24 13:09:12 -0800

    starting the cross sharing

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,23 @@
 class ProjectsController < ApplicationController
+
+    def index
+        @projects = Project.all
+        render json: @projects
+    end 
+
+    def create
+
+        @project = Project.new(project_params)
+  
+        if @project.save
+            render json: @project
+        end 
+    end 
+
+    private
+
+    def project_params
+        params.permit(:url)
+    end 
 end
+
