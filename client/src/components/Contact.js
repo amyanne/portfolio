535a1c46cdd99bea56bddaee358228bc179e948a
amyanne <amywestcott01@gmail.com>
2020-11-30 13:17:04 -0800

    another commit

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -12,9 +12,9 @@ export class Contact extends Component {
         return(
             <div className="page-contact">
                 <h1>Let's Talk!</h1>
-                <h2 className="details">amywestcott01@gmail.com</h2>
+                <h2 className="details">amywestcott01@gmail.com</h2><br></br>
                 <SocialMediaButtons
-                    links={['https://twitter.com/AmyWestcott6',' https://github.com/amyanne','https://www.linkedin.com/in/amy-westcott/','cats-and-coding.com']}
+                    links={['https://twitter.com/AmyWestcott6',' https://github.com/amyanne','https://www.linkedin.com/in/amy-westcott/']}
                     buttonStyle={{width: '55px', height: '55px', margin: '0px 26px', backgroundColor: '#000000', borderRadius: '50%'}}
                     iconStyle={{color: '#ffffff'}}
                     openNewTab={true}
