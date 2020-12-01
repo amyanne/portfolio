409725eeae324c34c0c332436138f3d6fb9917e2
amyanne <amywestcott01@gmail.com>
2020-12-01 09:58:46 -0800

    commit

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Redirect } from "react-router-dom";
+
 
 
 export class Nav extends Component {
@@ -11,7 +11,7 @@ export class Nav extends Component {
                     <il><Link to="/" className="tab">Home</Link></il>
                     <il><Link to="/projects" className="tab">Projects</Link></il>
                     <il><Link to="/contact" className="tab">Contact</Link></il>
-                    <il><a target="_blank" className="tab" href="https://cats-and-coding.com">Blog</a></il>
+                    <il><a target="noreferrer" className="tab" href="https://cats-and-coding.com">Blog</a></il>
                 </ul>
             </nav>
             
